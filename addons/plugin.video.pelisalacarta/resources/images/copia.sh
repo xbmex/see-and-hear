8de1d1a6c7d8bd74cc7362bd0efea5b081a2daba
martin.13xx@gmail.com <martin.13xx@gmail.com@07d0b3fa-501a-0d98-b63c-248ca6ab3891>
2015-01-22 08:07:52 +0000

    removug

--- addons/plugin.video.pelisalacarta/resources/images/copia.sh
+++
@@ -1,4 +0,0 @@
-scp squares/$1 root@hetzner04:/var/www/pelisalacarta.mimediacenter.info/squares/
-scp posters/$1 root@hetzner04:/var/www/pelisalacarta.mimediacenter.info/posters/
-scp banners/$1 root@hetzner04:/var/www/pelisalacarta.mimediacenter.info/banners/
-
